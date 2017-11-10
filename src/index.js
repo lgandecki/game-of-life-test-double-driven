14c713c275044a1430926888c34d47de902ab306
Łukasz Gandecki <lgandecki@css.edu>
2017-11-10 09:42:17 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const fs = require('fs');
+const test = 'sd';
+console.log('Gandecki test abc', test);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+const test = 'sd'
+
+console.log('Gandecki test abc', test)
